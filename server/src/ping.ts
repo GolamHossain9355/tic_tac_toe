84f18d409ddd094cac66053644a9de3d548835b4
Golam <rabby29hossain@gmail.com>
2023-06-01 15:35:52 -0400

    added a ping functionality

---
+++ server/src/ping.ts
@@ -0,0 +1,16 @@
+import fetch from "node-fetch"
+
+// Function to ping your server
+async function pingServer() {
+   try {
+      // Replace `https://your-server-url` with the actual URL of your server
+      const response = await fetch("https://your-server-url")
+      console.log(`Ping response: ${response.status}`)
+   } catch (error) {
+      console.error("Error pinging server:", error)
+   }
+}
+
+// Ping the server every 13 minutes (in milliseconds)
+const pingInterval = 13 * 60 * 1000
+setInterval(pingServer, pingInterval)
